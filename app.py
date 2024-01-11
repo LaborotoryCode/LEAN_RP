a038dcf5977f3e4af057e368b3517e5ebd642938
Ayaan Jain <ayaanjainsg@gmail.com>
2024-01-11 13:50:22 +0800

    i hate you

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import auth_functions
-from pages import LEAN_Teacher
+from . import LEAN_Teacher
 from streamlit_extras.switch_page_button import switch_page
 
 if 'user_info' not in st.session_state:
